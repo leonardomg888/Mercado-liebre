e3000ad775de608f08fc49da6c3d248edba583b1
leonardomg888 <leonardomg888@gmail.com>
2022-08-01 22:51:56 +0200

    formularios-animacion-

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ app.use( express.static( 'public' ));
 
 app.get( '/', ( req, res ) => res.sendFile( path.join( __dirname, 'views', 'home.html' )));
 
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html' )));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html' )));
+app.get('/home', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )));
+
+
 app.listen( PORT , () => console.log('Server running on port http://localhost:' + PORT));
